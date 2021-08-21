dcbab6a9e6cafdeac66e6064c864431bb06bdd84
Ari <ari@miniasp.com>
2021-08-21 11:22:52 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Page2Component } from './page2/page2.component';
+import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  // {
+  //   path: 'utilities',
+  //   children: [
+  //     { path: 'color', component: ColorComponent }
+  //   ]
+  // },
+  // { path: 'color', loadChildren: () => import('./utilities/color/color.module').then(m => m.ColorModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
